340cc6b0e66cef63e301dbaeb6d6f923fc0aaea0
bron <bronson.rameka@gmail.com>
2017-09-13 22:37:18 +0800

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdIconModule, MdToolbarModule, MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { DashComponent } from './dash/dash.component';
+
+import { RouterModule }   from '@angular/router';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MdButtonModule, 
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdIconModule,
+    RouterModule.forRoot([
+      {
+        path: 'dash',
+        component: DashComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
